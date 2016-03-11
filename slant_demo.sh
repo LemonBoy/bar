ce55ff7da50228153a094a96139cbef877ef8b3d
Nathan Isom <nathanisom27@gmail.com>
2016-03-10 21:17:53 -0600

    demo fun

--- slant_demo.sh
+++ slant_demo.sh
@@ -7,19 +7,26 @@ geom=400x20+10+10
 slant_fg='#a7a7a7'
 slant_bg='#3e3e3e'
 colors="%{F${slant_fg}}%{B${slant_bg}}"
+invert="%{B${slant_fg}}%{F${slant_bg}}"
 
 reset='%{B-}%{F-}'
 
 space()
 {
-	printf %${Len}s
+    printf %${Len}s
 }
+
+slant()
+{
+    echo -n "${colors}%{${1}${Len}}$(space)"
+}
+
 
 echo  " \
 ${colors}%{E${Len}}$(space)${reset} - \
 ${colors}%{e${Len}}$(space)${reset} - \
 ${colors}%{D${Len}}$(space)${reset} - \
 ${colors}%{d${Len}}$(space)${reset}     \
-${colors}%{d${Len}}$(space)%{B${slant_fg}}%{F${slant_bg}} ~ slants and shit ~ \
-${colors}%{E${Len}}$(space)${reset}"     \
-| ./lemonbar -g $geom -p -B "$slant_bg" -F "#FFFFFF"
+\
+$(slant d)${invert}slants$(slant E)${reset}and$(slant d)${invert}shit$(slant E)" \
+| ./lemonbar -g $geom -p -B "$slant_bg" -F "$slant_fg"
