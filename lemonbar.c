7e357c6694a2e41cdb9395da551145f0069f828f
dm9pZCAq <46228973+dm9pZCAq@users.noreply.github.com>
2024-08-31 20:06:06 +0300

    make bar always below other windows

--- lemonbar.c
+++ lemonbar.c
@@ -1318,7 +1318,7 @@ init (char *wm_name)
 
         // Make sure that the window really gets in the place it's supposed to be
         // Some WM such as Openbox need this
-        xcb_configure_window(c, mon->window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, (const uint32_t []){ mon->x, mon->y });
+        xcb_configure_window(c, mon->window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_STACK_MODE, (const uint32_t []){ mon->x, mon->y, XCB_STACK_MODE_BELOW });
 
         // Set the WM_NAME atom to the user specified value
         if (wm_name)
